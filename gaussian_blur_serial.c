203a510be7479152be1da4ef6e07ccbe8815e62e
Sean Patrick Malloy <spmalloy@ucdavis.edu>
2018-12-03 20:59:36 -0800

    Serial Implementation done!

--- gaussian_blur_serial.c
+++ gaussian_blur_serial.c
@@ -7,9 +7,9 @@
 ///////////////////
 //Multiply Kernal//
 ///////////////////
-void multiplyKernal(float* matrix, float* kernal, int order, int windowSizeX, int windowSizeY){
-	float temp[windowSizeX * windowSizeY];
-	memcpy(temp, matrix, (windowSizeX*windowSizeY) + 1;
+void multiplyKernal(unsigned char* matrix, float* kernal, int order, int windowSizeX, int windowSizeY){
+	unsigned char* temp = malloc(sizeof(char) * windowSizeX * windowSizeY);
+	memcpy(temp, matrix, (windowSizeX*windowSizeY));
 	int middle = ceil(order/2);
 
 	for(int y = 0; y < windowSizeY; y++){
@@ -20,20 +20,21 @@ void multiplyKernal(float* matrix, float* kernal, int order, int windowSizeX, in
 					int tempX = x - middle + x2, tempY = y - middle + y2;
 					if(tempX < 0){
 						tempX = 0;
-					}else if(tempX > windowSizeX){
+					}else if(tempX >= windowSizeX){
 						tempX = windowSizeX;
 					}
 					if(tempY < 0){
 						tempY = 0;
-					}else if(tempY > windowSizeY){
+					}else if(tempY >= windowSizeY){
 						tempY = windowSizeY;
 					}
 					sum += temp[(windowSizeX * tempY) + tempX] * kernal[(order * x2) + y2];
 				}
 			}
-			matrix[(windowSizeX * y) + x] = sum;
+			matrix[(windowSizeX * y) + x] = (unsigned char) sum;
 		}
 	}
+	free(temp);
 }
 
 
@@ -42,58 +43,78 @@ void multiplyKernal(float* matrix, float* kernal, int order, int windowSizeX, in
 /////////////////
 int main(int argc, char **argv)
 {
-	char* firstLine, secondLine, thirdLine;
-	int windowSizeX, windowSizeY;
-	float sigma, order, *matrix;
+	char firstLine[10];
+	int windowSizeX = 0, windowSizeY = 0, temp, order;
+	float sigma;
 
 	//Read in inputs, check if they are correct!
-	if(arc != 4){
+	if(argc != 4){
 		fprintf(stderr, "Usage: ./gaussian_blur_serial <input_file> <output_file> <sigma>\n");
 		exit(1);
 	}
 	FILE *fp;
-	fp = fopen(argv[1], 'r');
-	if(fp == NULL){
+	if((fp = fopen(argv[1], "rb")) == NULL){
 		fprintf(stderr, "Error: cannot open file %s\n", argv[1]);
 		exit(1);
 	}
-	fgets(firstLine, 4, fp);
-	if(strcmp(firstLine, "P5")){
-		fprintf(stderr, "Error: invalid PGM information\n");
+
+	if(fgets(firstLine, 4, fp) == NULL){
+		fprintf(stderr, "Error: cannot open file %s\n", argv[1]);
 		exit(1);
-	}
-	fgets(secondLine, 10, fp);
-	int n = sscanf(str, "%d %d", &windowSizeX, &windowSizeY);
-	if(n != 1){
-		fprintf(stderr, "Error: invalid PGM information\n");
 	}
 
-	fgets(thirdLine, 10, fp);
-	if(strcmp(thirdLine, "255")){
+	if(strcmp(firstLine, "P5\n")){
 		fprintf(stderr, "Error: invalid PGM information\n");
 		exit(1);
 	}
+	
+	if(!fscanf(fp,"%d", &windowSizeX) || !fscanf(fp,"%d", &windowSizeY)){
+		fprintf(stderr, "Error: cannot open file %s\n", argv[1]);
+		exit(1);
+	}
+
+	if(windowSizeX == 0 && windowSizeY == 0){
+		fprintf(stderr, "Error: invalid PGM information\n");
+		exit(1);
+	}
+
+	if(!fscanf(fp,"%d", &temp)){
+		fprintf(stderr, "Error: cannot open file %s\n", argv[1]);
+		exit(1);
+	}
+
+	if(temp != 255){
+		fprintf(stderr, "Error: invalid PGM information\n");
+		exit(1);
+	}
+
+	unsigned char* matrix = malloc(sizeof(unsigned char) * windowSizeX * windowSizeY);
+
+	if(fread(matrix, sizeof(unsigned char), windowSizeX*windowSizeY,fp) != windowSizeX*windowSizeY){
+		fprintf(stderr, "Error: invalid PGM pixels\n");
+		exit(1);
+	}
+
 	sigma = atof(argv[3]);
 	if(sigma == 0){
 		fprintf(stderr, "Error: invalid sigma value\n");
 		exit(1);
 	}
 	order = ceil(sigma * 6);
+	if(order%2 == 0){
+		order++;
+	}
 	if(order > windowSizeX || order > windowSizeY){
 		fprintf(stderr, "Error: sigma value too big for image size\n");
 		exit(1);
 	}
-
-	fread(matrix, sizeof(float),(windowSizeX * windowSizeY),fp);
-	if(ferror(fp)){
-		fprintf(stderr, "Error: invalid PGM pixels\n");
-		exit(1);
-	}
-
-	float kernal[order*order];
+	
+	int middle = ceil(order/2);
+	float* kernal = malloc(sizeof(float) * order * order);
 	for(int y = 0; y < order; y++){
 		for(int x = 0; x < order; x++){
-			kernal[(order * y) + x] = (1/(2*M_PI*(pow(sigma,2)))) * (pow(M_E, -((pow(x,2) + pow(y,2))/(2*pow(sigma,2)))));
+			int x2 = x - middle, y2 = y - middle;
+			kernal[(order * y) + x] = (1/(2*M_PI*(pow(sigma,2)))) * (pow(M_E, -((pow(x2,2) + pow(y2,2))/(2*pow(sigma,2)))));
 		}
 	}
 
@@ -116,16 +137,18 @@ int main(int argc, char **argv)
     fd = fopen(name, "w+");
     fprintf(fd, "P5\n");
     fprintf(fd, "%d %d\n", windowSizeX, windowSizeY);
-    fprintf(fd, "255\n",);
-    int count = 0;
+    fprintf(fd, "255\n");
 
     for(int y = 0; y < windowSizeY; y++) {
         for(int x = 0; x < windowSizeX; x++) {
-            fprintf(fp, "%d ", matrix[(windowSizeX * y) + x]);
+            fprintf(fd, "%c", matrix[(windowSizeX * y) + x]);
         }
-		fprintf(fp, "\n");
     }
 
+	fclose(fp);
+	fclose(fd);
+	free(kernal);
+	free(matrix);
 
 	return 0;
 }
